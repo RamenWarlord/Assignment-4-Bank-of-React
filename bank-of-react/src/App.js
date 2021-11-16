3dc8de375402eb48611b59a845b95d109c98782a
ramenwarlord <ivanyeung12345@gmail.com>
2021-11-16 01:20:09 -0500

    accbalance prop to routes

--- bank-of-react/src/App.js
+++ bank-of-react/src/App.js
@@ -1,14 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
+
 function App() {
+  const [balance, changeBalance] = useState(14568.27);
+
   return (
     <Router>
       <Switch>
         <Route path="/">
-          <Home></Home>
+          <Home accountBalance={balance}></Home>
         </Route>
       </Switch>
     </Router>
---
+++ bank-of-react/src/components/AccountBalance.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+function AccountBalance(props) {
+  return <div>Balance: {props.accountBalance}</div>;
+}
+
+export default AccountBalance;
