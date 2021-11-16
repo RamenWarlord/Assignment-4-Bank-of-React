e3663dfa01c1bd79121280c60361756c6829f89b
ramenwarlord <ivanyeung12345@gmail.com>
2021-11-16 02:51:49 -0500

    debits component init

---
+++ bank-of-react/src/components/Debits.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+function Debits() {
+  return (
+    <div>
+      <h1>Debits</h1>
+      <Link to="/">Return to Home</Link>
+    </div>
+  );
+}
+
+export default Debits;
