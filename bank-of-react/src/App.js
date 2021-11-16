c9991f044cf6d5040767ede2856b199a5685e894
ramenwarlord <ivanyeung12345@gmail.com>
2021-11-16 03:01:58 -0500

    impl basic api call method

--- bank-of-react/src/App.js
+++ bank-of-react/src/App.js
@@ -42,7 +42,20 @@ function App() {
   //react hook useEffect when passing an empty dependency array makes it act like componentDidMount where it only runs once
   useEffect(() => {
     //insert api requests here
+    let debitResponse = apiCall("https://moj-api.herokuapp.com/debits");
+    let creditResponse = apiCall("https://moj-api.herokuapp.com/credits");
   }, []);
+
+  //api call function
+  async function apiCall(apiUrl) {
+    try {
+      let response = await fetch(apiUrl); //'https://moj-api.herokuapp.com/debits'
+      let jsonResponse = await response.json();
+      return jsonResponse;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <Router>
