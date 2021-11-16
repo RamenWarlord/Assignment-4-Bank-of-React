3a650028e0d12ede3cdd080eeb27f6d30e57aa8f
ramenwarlord <ivanyeung12345@gmail.com>
2021-11-16 02:28:39 -0500

    fixed and tested mocklogin works

--- bank-of-react/src/components/Login.js
+++ bank-of-react/src/components/Login.js
@@ -1,46 +1,50 @@
-import React, {useState} from 'react';
-import { Redirect } from 'react-router-dom';
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
 
-function Login(props){
-    const[user, updateUser] = useState({
-        userName = '',
-        password = '',
-    });
-    const[redirect, confirmRedirect] = useState(false);
+function Login(props) {
+  const [user, updateUser] = useState({
+    userName: "",
+    password: "",
+  });
+  const [redirect, confirmRedirect] = useState(false);
 
-    const handleChange = (e) =>{
-        updateUser((prevState) => ({
-            ...prevState,
-            [e.target.name]: e.target.value
-        }))
-    };
-    
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        props.mockLogIn(user);
-        confirmRedirect(true);
-    }
+  const handleChange = (e) => {
+    updateUser((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
-    if(redirect){
-        return(<Redirect to="/userProfile"/>);
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.mockLogIn(user);
+    confirmRedirect(true);
+  };
 
-    return(
+  if (redirect) {
+    return <Redirect to="/userProfile" />;
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
         <div>
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            <label htmlFor="userName">User Name</label>
-            <input type="text" name="userName" onChange={this.handleChange} value={this.state.user.userName} />
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" />
-          </div>
-          <button>Log In</button>
-        </form>
-      </div>
-    );
-
+          <label htmlFor="userName">User Name</label>
+          <input
+            type="text"
+            name="userName"
+            onChange={handleChange}
+            value={user.userName}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" />
+        </div>
+        <button>Log In</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
