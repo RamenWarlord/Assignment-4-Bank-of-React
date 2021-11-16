1fea5c9e1865acef280988f699638b731c0a7f83
ramenwarlord <ivanyeung12345@gmail.com>
2021-11-16 01:34:33 -0500

    impl dummy user profile

--- bank-of-react/src/App.js
+++ bank-of-react/src/App.js
@@ -3,15 +3,25 @@ import "./App.css";
 import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   const [balance, changeBalance] = useState(14568.27);
-
+  const [user, updateUserInfo] = useState({
+    userName: "joe_shmo",
+    memberSince: "07/23/96",
+  });
   return (
     <Router>
       <Switch>
-        <Route path="/">
-          <Home accountBalance={balance}></Home>
+        <Route exact path="/">
+          <Home accountBalance={balance} />
+        </Route>
+        <Route path="/userProfile">
+          <UserProfile
+            userName={user.userName}
+            memberSince={user.memberSince}
+          />
         </Route>
       </Switch>
     </Router>
