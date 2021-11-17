213bb66f33484ab19f454f234a91328dc45e46dd
ramenwarlord <ivanyeung12345@gmail.com>
2021-11-17 18:13:03 -0500

    added bootstrap, basic grid layout for home pg

--- bank-of-react/src/components/Home.js
+++ bank-of-react/src/components/Home.js
@@ -1,15 +1,25 @@
 import React from "react";
 import AccountBalance from "./AccountBalance";
 import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
 function Home(props) {
   return (
-    <div>
-      <h1>Bank of React</h1>
-
-      <Link to="/userProfile">User Profile</Link>
-      <Link to="/debits">Debits</Link>
-      <Link to="/credits">Credits</Link>
-      <AccountBalance accountBalance={props.accountBalance} />
+    <div className="container">
+      <h1 className="d-flex justify-content-center flex-wrap">Bank of React</h1>
+      <div className="d-flex justify-content-center flex-wrap">
+        <AccountBalance accountBalance={props.accountBalance} />
+      </div>
+      <div className="row">
+        <div className="col d-flex justify-content-center flex-wrap">
+          <Link to="/userProfile">User Profile</Link>
+        </div>
+        <div className="col d-flex justify-content-center flex-wrap">
+          <Link to="/debits">Debits</Link>
+        </div>
+        <div className="col d-flex justify-content-center flex-wrap">
+          <Link to="/credits">Credits</Link>
+        </div>
+      </div>
     </div>
   );
 }
