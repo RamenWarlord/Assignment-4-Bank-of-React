f997ffdf9415348259bfe7c59350d7eea68189cd
ramenwarlord <ivanyeung12345@gmail.com>
2021-11-16 23:43:02 -0500

    copied over debit return format

--- bank-of-react/src/components/Credits.js
+++ bank-of-react/src/components/Credits.js
@@ -2,16 +2,53 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import AccountBalance from "./AccountBalance";
 
-function Credits(props){
-    const [credits, updateCredits] = useState(props.creditInfo);
-    const [newCredit, updateNewCredit] = useState({
-      id: "",
-      description: "",
-      amount: "",
-      date: "",
-    });
-    
-    return();
+function Credits(props) {
+  const [credits, updateCredits] = useState(props.creditInfo);
+  const [newCredit, updateNewCredit] = useState({
+    id: "",
+    description: "",
+    amount: "",
+    date: "",
+  });
+
+  return (
+    <div>
+      <h1>Credits</h1>
+      <AccountBalance accountBalance={props.accountBalance} />
+      <ul>
+        {credits.map((credit) => (
+          <li key={credit.id}>
+            Item: {credit.description} <br />
+            Cost: ${credit.amount} <br />
+            Date: {credit.date}
+          </li>
+        ))}
+      </ul>
+
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="description">Description</label>
+            <input
+              type="text"
+              name="description"
+              onChange={handleChange}
+              value={newCredit.description}
+            />
+            <label htmlFor="amount">amount</label>
+            <input
+              type="number"
+              name="amount"
+              onChange={handleChange}
+              value={newCredit.amount}
+            />
+          </div>
+          <button type="submit">Add new credit</button>
+        </form>
+      </div>
+      <Link to="/">Return to Home</Link>
+    </div>
+  );
 }
 
 export default Credits;
