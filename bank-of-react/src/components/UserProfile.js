64af9d3e8a20cc7b08f673ecad5ad8cbe5fd9d3b
ramenwarlord <ivanyeung12345@gmail.com>
2021-11-17 18:40:42 -0500

    basic bootstrap css for userprofile pg

--- bank-of-react/src/components/UserProfile.js
+++ bank-of-react/src/components/UserProfile.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
 
 function UserProfile(props) {
   return (
-    <div>
-      <h1>User Profile</h1>
+    <div className="container">
+      <h1 className="d-flex justify-content-center flex-wrap">User Profile</h1>
 
-      <div>Username: {props.userName}</div>
-      <div>Member Since: {props.memberSince}</div>
-
-      <Link to="/">Return to Home</Link>
+      <div className="d-flex justify-content-center flex-wrap">
+        Username: {props.userName}
+      </div>
+      <div className="d-flex justify-content-center flex-wrap">
+        Member Since: {props.memberSince}
+      </div>
+      <div className="d-flex justify-content-center flex-wrap">
+        <Link to="/">Return to Home</Link>
+      </div>
     </div>
   );
 }
