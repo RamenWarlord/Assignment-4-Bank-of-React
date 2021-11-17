04d494ede959786776c068d2199e489a107bb318
ramenwarlord <ivanyeung12345@gmail.com>
2021-11-16 23:39:22 -0500

    state init

--- bank-of-react/src/components/Credits.js
+++ bank-of-react/src/components/Credits.js
@@ -3,6 +3,14 @@ import { Link } from "react-router-dom";
 import AccountBalance from "./AccountBalance";
 
 function Credits(props){
+    const [credits, updateCredits] = useState(props.creditInfo);
+    const [newCredit, updateNewCredit] = useState({
+      id: "",
+      description: "",
+      amount: "",
+      date: "",
+    });
+    
     return();
 }
 
